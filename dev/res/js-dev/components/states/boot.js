75ba3870035e2bce9e22bc254302dc3cc91b7579
Ilja Biryukov <uncle.funkay@gmail.com>
2018-05-13 20:23:55 +0300

    Project Update

--- dev/res/js-dev/components/states/boot.js
+++ dev/res/js-dev/components/states/boot.js
@@ -35,17 +35,17 @@ export default function init(game) {
     },
 
     preload() {
-      game.load.spritesheet('charJump', '/assets/sprites/charJump.png', 32, 32, 7);
-      game.load.spritesheet('charWalk', '/assets/sprites/charWalk.png', 32, 32, 4);
-      game.load.spritesheet('dust', '/assets/sprites/dust.png', 32, 32, 3);
-      game.load.spritesheet('explosion', '/assets/sprites/explosion.png', 38, 32, 8);
-      game.load.spritesheet('weapon', '/assets/sprites/weapon.png', 36, 32, 6);
-      game.load.spritesheet('zombie1', '/assets/sprites/zombie1.png', 32, 32, 6);
-      game.load.spritesheet('zombie2', '/assets/sprites/zombie2.png', 32, 32, 6);
-      game.load.image('cloud1', '/assets/sprites/cloud1.png');
-      game.load.image('cloud2', '/assets/sprites/cloud2.png');
-      game.load.image('crate', '/assets/sprites/crate.png');
-      game.load.image('floor', '/assets/sprites/floor.png');
+      game.load.spritesheet('charJump', 'assets/sprites/charJump.png', 32, 32, 7);
+      game.load.spritesheet('charWalk', 'assets/sprites/charWalk.png', 32, 32, 4);
+      game.load.spritesheet('dust', 'assets/sprites/dust.png', 32, 32, 3);
+      game.load.spritesheet('explosion', 'assets/sprites/explosion.png', 38, 32, 8);
+      game.load.spritesheet('weapon', 'assets/sprites/weapon.png', 36, 32, 6);
+      game.load.spritesheet('zombie1', 'assets/sprites/zombie1.png', 32, 32, 6);
+      game.load.spritesheet('zombie2', 'assets/sprites/zombie2.png', 32, 32, 6);
+      game.load.image('cloud1', 'assets/sprites/cloud1.png');
+      game.load.image('cloud2', 'assets/sprites/cloud2.png');
+      game.load.image('crate', 'assets/sprites/crate.png');
+      game.load.image('floor', 'assets/sprites/floor.png');
     },
 
   };
